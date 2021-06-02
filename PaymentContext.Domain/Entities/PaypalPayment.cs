23eb2eb908633fad612f786cfb97f043afdbbe45
SHILTON PEREIRA SANTIAGO - 900000968 <shilton.santiago@animaeducacao.com.br>
2021-06-02 17:31:05 -0300

    Usando Value Objects

--- PaymentContext.Domain/Entities/PaypalPayment.cs
+++ PaymentContext.Domain/Entities/PaypalPayment.cs
@@ -1,10 +1,11 @@
 using System;
+using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Domain.Entities
 {
     public class PaypalPayment : Payment
     {
-        public PaypalPayment(string transactionCode, DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, string email, string document, string address) 
+        public PaypalPayment(string transactionCode, DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, Email email, Document document, Address address) 
                             : base(paidDate, expireDate, total, totalPaid, payer, email, document, address)
         {
             TransactionCode = transactionCode;
